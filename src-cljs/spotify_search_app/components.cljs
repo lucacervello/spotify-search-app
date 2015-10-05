6b7158685a9e1c04cc5259c4ad005f3e2f930ffa
lucacervello <luca.cervello@gmail.com>
2015-10-05 18:33:56 +0200

    move card because there is a warning

--- src-cljs/spotify_search_app/components.cljs
+++ src-cljs/spotify_search_app/components.cljs
@@ -16,6 +16,19 @@
 ;; -------------------------
 ;; Components
 
+
+(defn card [item]
+  (let [img-url (if (seq (:images item))
+                  (-> item :images first :url)
+                  default-icon-url)]
+    [:div.media
+     [:div.media-left
+      [:img.media-object {:src img-url
+                          :width 64
+                          :height 64}]]
+     [:div.media-body [:p.media-heading (:name item)]]]))
+
+
 (defn search [app-state]
   [:div.input-group
    [:span.input-group-btn
@@ -45,14 +58,3 @@
           [:a.list-group-item
            {:href (str "#/track/" (:id item))}
            [card item]])]]])])
-
-(defn card [item]
-  (let [img-url (if (seq (:images item))
-              (-> item :images first :url)
-              default-icon-url)]
-    [:div.media
-     [:div.media-left
-      [:img.media-object {:src img-url
-                          :width 64
-                          :height 64}]]
-     [:div.media-body [:p.media-heading (:name item)]]]))
